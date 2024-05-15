a8d6200f1c2a16fe50ad27c5170bf5acabbf9fc0
luwentao <wlu4@ualbera.ca>
2024-05-14 22:06:45 -0600

    image download

--- bug_report_scraper/bug_report_scraper/spiders/bug_spider.py
+++ bug_report_scraper/bug_report_scraper/spiders/bug_spider.py
@@ -1,5 +1,8 @@
 import scrapy
 from bs4 import BeautifulSoup
+from bug_report_scraper.items import BugReportItem
+from urllib.parse import urljoin
+
 
 # class BugSpider(scrapy.Spider):
 #     name = 'bug_spider'
@@ -37,40 +40,45 @@ from bs4 import BeautifulSoup
 #         for link in thread_links:
 #             yield response.follow(link, self.parse_thread)
 
-
 class BugSpider(scrapy.Spider):
     name = 'bug_spider'
-    start_urls = ['https://answers.ea.com/t5/Bug-Reports/bd-p/wrc-bug-reports-en/page/2']
-
+    start_urls = ['https://answers.ea.com/t5/Bug-Reports/bd-p/wrc-bug-reports-en']
+    domain = 'https://answers.ea.com'
     def parse_thread(self, response):
-        # Convert the Scrapy response to a BeautifulSoup object
         soup = BeautifulSoup(response.text, 'lxml')
-
-        # Assuming the original post is the first div with class 'lia-message-body-content'
         original_post_div = soup.find('div', class_='lia-message-body-content')
-
-        # Check if the div is found
+        
         if original_post_div:
-            # Extract all text, including handling nested tags
             original_post_text = ' '.join(original_post_div.stripped_strings)
         else:
             original_post_text = "Original post not found."
 
-        # Extract image URLs using Scrapy's CSS selectors
-        # This assumes images are linked in <a> tags with class 'attachment-link'
-        image_urls = response.css('a.lia-link-navigation.attachment-link::attr(href)').getall()
+        images = soup.find_all('a', class_='lia-link-navigation attachment-link')
+        # print(images)
+        base_url = self.domain
+        image_urls =[]
+        for img in images:
+            download_url = img.find('span')['li-download-url']
+            full_url = urljoin(base_url, download_url)
+            # print(full_url)
+            image_urls.append(full_url)
 
-        # if no image, skip
         if not image_urls:
             return
+
+        item = BugReportItem()
+        item['description'] = original_post_text
+        item['image_urls'] = image_urls
+        yield item
         
-        yield {
-            'description': original_post_text,
-            'image_urls': image_urls
-        }
+
 
     def parse(self, response):
-        # Loop through all thread links on the page
         thread_links = response.css('a.page-link.lia-link-navigation.lia-custom-event::attr(href)').getall()
         for link in thread_links:
             yield response.follow(link, self.parse_thread)
+        
+        next_page = response.css('a.lia-link-navigation.lia-js-data-pageNum-2.lia-custom-event::attr(href)').get()
+        
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
---
+++ request.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+from urllib.parse import urljoin
+# url = 'https://answers.ea.com/t5/Bug-Reports/Textures-Medium-and-below-are-very-blurry-in-all-Menus/td-p/13698266?attachment-id=816308'
+url = 'https://answers.ea.com/t5/Bug-Reports/Textures-Medium-and-below-are-very-blurry-in-all-Menus/td-p/13698266'
+domain = 'https://answers.ea.com'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+images = soup.find_all('a', class_='lia-link-navigation attachment-link')
+print(images)
+base_url = domain
+
+for img in images:
+    download_url = img.find('span')['li-download-url']
+    full_url = urljoin(base_url, download_url)
+    print(full_url)
+    image = requests.get(full_url)
+    with open('image.jpg', 'wb') as file:
+        file.write(image.content)
+        file.close()
+
+# save html 
+# with open('test.html', 'w') as file:
+#     file.write(response.text)
+#     file.close()
