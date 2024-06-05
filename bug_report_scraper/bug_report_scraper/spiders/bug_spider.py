a5924985960f8baf5141a4e7f7ecfb4fc5969fbf
Wentao Lu <39390293+lwt0000@users.noreply.github.com>
2024-06-05 10:11:23 -0600

    still contains bug, checkpoints

--- bug_report_scraper/bug_report_scraper/spiders/bug_spider.py
+++ bug_report_scraper/bug_report_scraper/spiders/bug_spider.py
@@ -3,81 +3,37 @@ from bs4 import BeautifulSoup
 from bug_report_scraper.items import BugReportItem
 from urllib.parse import urljoin
 
-# class BugSpider(scrapy.Spider):
-#     name = 'bug_spider'
-#     start_urls = ['https://answers.ea.com/t5/Bug-Reports/bd-p/wrc-bug-reports-en']
-#     domain = 'https://answers.ea.com'
-#     def parse_thread(self, response):
-#         soup = BeautifulSoup(response.text, 'lxml')
-#         original_post_div = soup.find('div', class_='lia-message-body-content')
-        
-#         if original_post_div:
-#             original_post_text = ' '.join(original_post_div.stripped_strings)
-#         else:
-#             original_post_text = "Original post not found."
 
-#         images = soup.find_all('a', class_='lia-link-navigation attachment-link')
-#         # print(images)
-#         base_url = self.domain
-#         image_urls =[]
-#         for img in images:
-#             download_url = img.find('span')['li-download-url']
-#             full_url = urljoin(base_url, download_url)
-#             # print(full_url)
-#             image_urls.append(full_url)
-
-#         if not image_urls:
-#             return
-        
-
-#         #find the number of people who have the same issue
-#         num_pepole_has_same_issue = soup.find('a', class_='lia-link-navigation lia-rating-value-summary')
-#         if num_pepole_has_same_issue:
-#             num_pepole_has_same_issue = num_pepole_has_same_issue.text[0]
-        
-
-#         item = BugReportItem()
-#         item['description'] = original_post_text
-#         item['image_urls'] = image_urls
-#         item['original_post_url'] = response.url
-#         item['num_pepole_has_same_issue'] = int(num_pepole_has_same_issue)
-#         item['images_names'] = []
-#         yield item
-        
-
-
-#     def parse(self, response):
-#         thread_links = response.css('a.page-link.lia-link-navigation.lia-custom-event::attr(href)').getall()
-#         for link in thread_links:
-#             yield response.follow(link, self.parse_thread)
-        
-#         next_page = response.css('a.lia-link-navigation.lia-js-data-pageNum-2.lia-custom-event::attr(href)').get()
-        
-#         if next_page is not None:#comment this line if don't want to scrape all pages
-#             yield response.follow(next_page, callback=self.parse)
-
-
-
-
-
-import scrapy
-from bs4 import BeautifulSoup
-from bug_report_scraper.items import BugReportItem
-from urllib.parse import urljoin
 
 class BugSpider(scrapy.Spider):
     name = 'bug_spider'
     domain = 'https://answers.ea.com'
-    
+    main_page_id = 2 #used for pagination of the main bug report page
+    post_page_id = 2 #used for pagination of the post page
     def __init__(self, start_urls=None, game_name=None, *args, **kwargs):
         super(BugSpider, self).__init__(*args, **kwargs)
         if start_urls:
             self.start_urls = [start_urls]
         self.game_name = game_name
+    def check_pages_in_each_thread(self, response):
+        soup = BeautifulSoup(response.text, 'lxml')
+        #look for images
+        images = soup.find_all('a', class_='lia-link-navigation attachment-link')
+        base_url = self.domain
+        image_urls =[]
+        for img in images:
+            download_url = img.find('span')['li-download-url']
+            #if the last 3 characters are not png, jpg or jpeg, skip the image
+            if download_url[-3:] not in ['png', 'jpg', 'jpeg','bmp','heic']:
+                #this need improvement
+                continue
+            full_url = urljoin(base_url, download_url)
+            image_urls.append(full_url)
+        return image_urls
 
     def parse_thread(self, response):
         soup = BeautifulSoup(response.text, 'lxml')
-        original_post_div = soup.find('div', class_='lia-message-body-content')
+        original_post_div = soup.find('div', class_='lia-message-body-content')#find the first post in this thread  
         
         if original_post_div:
             original_post_text = ' '.join(original_post_div.stripped_strings)
@@ -91,9 +47,18 @@ class BugSpider(scrapy.Spider):
             download_url = img.find('span')['li-download-url']
             #if the last 3 characters are not png, jpg or jpeg, skip the image
             if download_url[-3:] not in ['png', 'jpg', 'jpeg','bmp','heic']:#elimate possible .txt file or other file when bug report usually contains
+                #this need improvement
                 continue
             full_url = urljoin(base_url, download_url)
             image_urls.append(full_url)
+
+        # scrape the other pages inside this post if there are any
+        link = 'a.lia-link-navigation.lia-js-data-pageNum-{id}.lia-custom-event::attr(href)'.format(id=self.main_page_id)
+        next_page = response.css(link).get()
+        if next_page is not None:
+            lst = self.check_pages_in_each_thread(next_page)
+            #appeend
+            image_urls.extend(lst)
 
         if not image_urls:
             return
@@ -106,16 +71,25 @@ class BugSpider(scrapy.Spider):
         item['description'] = original_post_text
         item['image_urls'] = image_urls
         item['original_post_url'] = response.url
-        item['num_pepole_has_same_issue'] = int(num_pepole_has_same_issue)
+        #try converting the number of people to int, if it fails, set it to 0, not every post has this, ex. replys under the original post
+        try:
+            item['num_pepole_has_same_issue'] = int(num_pepole_has_same_issue)
+        except:
+            item['num_pepole_has_same_issue'] = 0
         item['images_names'] = []
+
+        #reset page id for the next thread
+        self.post_page_id = 2
         yield item
 
     def parse(self, response):
         thread_links = response.css('a.page-link.lia-link-navigation.lia-custom-event::attr(href)').getall()
         for link in thread_links:
             yield response.follow(link, self.parse_thread)
-        
-        next_page = response.css('a.lia-link-navigation.lia-js-data-pageNum-2.lia-custom-event::attr(href)').get()
+        #go to next page in the bug report main page
+        link = 'a.lia-link-navigation.lia-js-data-pageNum-{id}.lia-custom-event::attr(href)'.format(id=self.main_page_id)
+        self.main_page_id += 1
+        next_page = response.css(link).get()
         
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
